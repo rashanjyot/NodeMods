149fbe608bf637ded063654a7e3e0b31d9ae4351
rashanjyot <rashanjyot@yahoo.com>
2017-08-09 15:39:18 +0530

    dkos

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var express=require('express')
 var app=express();
 app.set('port',(process.env.PORT||5000))
 app.set('view engine','ejs');
-app.use('/',express.static(__dirname+"/public"))
+app.use('/',express.static(__dirname+"/public/abc.html"))
 
 
 app.listen(app.get('port'),function () {
